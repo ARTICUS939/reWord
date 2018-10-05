61d53406041f7bd09d43eaf177834479bfc25978
ARTICUS939 <articus_mib@mail.ru>
2018-10-05 17:31:14 +0300

    first

--- reWord/Program.cs
+++ reWord/Program.cs
@@ -10,6 +10,42 @@ namespace reWord
     {
         static void Main(string[] args)
         {
+
+            
+            Console.WriteLine("Введите строку");
+
+            string word = Console.ReadLine();
+
+            int length = word.Length;
+
+            string reword = "";
+
+            for (int i = length; i >= 1; i--)
+
+            {
+              reword += word[i - 1];
+            }
+
+            Console.WriteLine(reword);
+           
+
+
+
+         /* используя массив
+
+            Console.Write("Введите строку: ");
+
+            string word = Console.ReadLine(); 
+
+            char[] reword = word.ToCharArray(); 
+
+            int last = word.Length - 1; 
+
+            for (int n = last; n >= 0; n--) Console.Write(word[n]); 
+         */
+
+            Console.ReadLine();
+
         }
     }
 }
